caf806e9387231ca4b0c08af6ad8338ed6aedcd4
Sebastian See <sebastian.see@mlcinsurance.com.au>
2025-09-12 11:04:08 +1000

    build lambda shell script

---
+++ build_lambda.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define variables
+PACKAGE_DIR="lambda_package"
+ZIP_FILE="lambda_deploy.zip"
+LAMBDA_FILE="fileparser.py"
+
+# Clean up previous builds
+rm -rf $PACKAGE_DIR $ZIP_FILE
+
+# Create package directory
+mkdir $PACKAGE_DIR
+
+# Install dependencies into the package directory
+pip install -r requirements.txt -t $PACKAGE_DIR
+
+# Copy your lambda function into the package
+cp $LAMBDA_FILE $PACKAGE_DIR/
+
+# Zip the contents
+cd $PACKAGE_DIR
+zip -r ../$ZIP_FILE .
+cd ..
+
+echo "✅ Lambda deployment package created: $ZIP_FILE"
