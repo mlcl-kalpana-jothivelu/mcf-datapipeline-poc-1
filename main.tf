4518c2414cdd39f66cc920a20d5b9f908660222f
Kalpana Devi Jothivelu <30466075+kalpanadevi03@users.noreply.github.com>
2025-09-17 15:23:01 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,6 +85,23 @@ resource "aws_iam_role_policy" "s3_eventbridge_policy" {
   })
 }
 
+# Security Group for Lambda
+resource "aws_security_group" "lambda" {
+  name_prefix = "${var.project_name}-lambda-"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-lambda-sg"
+  }
+}
+
 # Lambda function
 resource "aws_lambda_function" "main" {
   function_name    = "${var.project_name}-lambda-processor"
@@ -94,6 +111,10 @@ resource "aws_lambda_function" "main" {
   filename        = "${path.module}/lambda_function.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
 
+  vpc_config {
+    subnet_ids         = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+    security_group_ids = [aws_security_group.lambda.id]
+  }
   # handler         = "lambda_function.lambda_handler"
   # filename         = data.archive_file.lambda_zip.output_path
   # source_code_hash = data.archive_file.lambda_zip.output_base64sha256
